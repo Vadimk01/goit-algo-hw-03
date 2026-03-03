c5f45cfbe19d53fb759b4ce385acb076a22a5a97
Vadimk01 <east4445@gmail.com>
2026-03-03 19:34:43 +0200

    task2 has been fixed

--- task2.py
+++ task2.py
@@ -1,12 +1,23 @@
 import random
 
-def get_numbers_ticket(min, max, quantity):
-    if max < 1:
-        return"число не може бути меншим ніж 1"
-    if max > 1000:
-        return "число не може бути більшим за 1000"
+def get_numbers_ticket(min_num, max_num, quantity):
+    # Перевірка валідності параметрів
+    if (
+        min_num < 1 or
+        max_num > 1000 or
+        min_num > max_num or
+        quantity < 1 or
+        quantity > (max_num - min_num + 1)
+    ):
+        return []
 
-    return  sorted(random.sample(range(min, max),  quantity))
+    # Генерація унікальних чисел (включаючи max)
+    numbers = random.sample(range(min_num, max_num + 1), quantity)
+    
+    return sorted(numbers)
 
-
-print(get_numbers_ticket(int(input("Введіть мінімальне число:")), int(input("Введіть максимальне число:")), int(input("Введіть к-ть:"))))
+print(get_numbers_ticket(
+    int(input("Введіть мінімальне число: ")),
+    int(input("Введіть максимальне число: ")),
+    int(input("Введіть к-ть: "))
+))
